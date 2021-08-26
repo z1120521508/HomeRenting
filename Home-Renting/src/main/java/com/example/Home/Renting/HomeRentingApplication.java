be3a68de33f530faf9627c240429327744657675
OldBlack <56985834+z1120521508@users.noreply.github.com>
2021-08-26 16:58:50 +0800

    Add files via upload

---
+++ Home-Renting/src/main/java/com/example/Home/Renting/HomeRentingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Home.Renting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRentingApplication.class, args);
+	}
+
+}
---
+++ Home-Renting/src/test/java/com/example/Home/Renting/HomeRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Home.Renting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRentingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
