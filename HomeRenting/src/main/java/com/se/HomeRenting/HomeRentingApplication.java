5e9ec825a9ba4b346d32b61f11faeba8673c56cb
OldBlack <56985834+z1120521508@users.noreply.github.com>
2021-08-29 15:56:54 +0800

    Add files via upload

---
+++ HomeRenting/src/main/java/com/se/HomeRenting/HomeRentingApplication.java
@@ -0,0 +1,13 @@
+package com.se.HomeRenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeRentingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeRentingApplication.class, args);
+	}
+
+}
