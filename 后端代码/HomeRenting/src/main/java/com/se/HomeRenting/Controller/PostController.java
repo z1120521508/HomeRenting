987316cc97344ae90336a32aac53d44c9bf3693d
沈 <73063351+sgw-swag@users.noreply.github.com>
2021-09-12 18:03:06 +0800

    Add files via upload

--- 后端代码/HomeRenting/src/main/java/com/se/HomeRenting/Controller/PostController.java
+++ 后端代码/HomeRenting/src/main/java/com/se/HomeRenting/Controller/PostController.java
@@ -14,13 +14,23 @@ public class PostController {
     PostService postService;
 
     @GetMapping("/list")
-    public List<Post> getPost(){
+    public List<Post> getAllPost(){
         return postService.getALL();
+    }
+
+    @GetMapping("/list/{tid}")
+    public List<Post> getPost(@PathVariable("tid") int tid){
+        return postService.getPost(tid);
     }
 
     @PostMapping("/resource")
     public String putPost(@RequestBody Post post){
         postService.putPost(post);
         return "发贴成功";
+    }
+
+    @DeleteMapping("/resource/{tid}")
+    public void deleteText(@PathVariable("tid") int tid){
+        postService.deletePost(tid);
     }
 }
